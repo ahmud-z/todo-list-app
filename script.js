87389443db4ec9dd3933536d004bade9033ea0ec
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-04-29 10:18:05 +0600

    updated

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function addTask() {
         li.innerHTML = todoInput.value;
 
         let span = document.createElement('span');
-        span.innerHTML = '&#10005;';
+        span.innerHTML = '<i class="bi bi-trash"></i>';
         li.appendChild(span);
         li.classList.add('list-item');
         listContainer.appendChild(li);
@@ -26,6 +26,9 @@ listContainer.addEventListener('click', function (event) {
     } else if (event.target.tagName === 'SPAN') {
         event.target.parentElement.remove();
         storeDataInLocal();
+    } else if (event.target.tagName === 'I') {
+        event.target.parentElement.parentElement.remove();
+        storeDataInLocal();
     }
 }, false);
 
@@ -36,7 +39,4 @@ function showSyncData() {
     listContainer.innerHTML = localStorage.getItem('todoData');
 }
 
-
 showSyncData();
-
-
