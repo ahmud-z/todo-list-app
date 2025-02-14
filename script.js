5d7e5a8c6cda942aacfe99afe2af577063f4b451
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2025-02-14 10:43:13 +0600

    UI updated

--- script.js
+++ script.js
@@ -2,17 +2,34 @@ const todoInput = document.getElementById('todoInput');
 const addBtn = document.getElementById('addBtn');
 const listContainer = document.getElementById('listContainer');
 
+const date = new Date()
+const d = new Date();
+const yr = d.getFullYear()
+
 function addTask() {
     if (todoInput.value === '') {
         alert('must have to enter some text');
     } else {
+        let dateContainer = document.createElement('p')
+        // dateContainer.innerHTML = (yr + "-" + (d.getMonth() + 1) + "-" + d.getDate())
+        div1 = document.createElement('div')
+        div2 = document.createElement('div')
+
+
         let li = document.createElement('li');
         li.innerHTML = todoInput.value;
 
         let span = document.createElement('span');
         span.innerHTML = '<i class="bi bi-trash"></i>';
+        
+        div1.appendChild(dateContainer)
+
+        li.appendChild(dateContainer);
         li.appendChild(span);
         li.classList.add('list-item');
+        dateContainer.classList.add('dateContainer')
+        
+        div2.classList.add('test')
         listContainer.appendChild(li);
     }
     todoInput.value = '';
